d7d38367592f197b0f9f6df359042fc9bec1f524
Gerasim Gerasimov <dialix@yandex.ru>
2021-03-18 18:20:32 +0700

    получил расшифровку кол-ва событий в карточке. Надо занятся стилизацией

--- src/event-log-reader/event-card/EventsCounters.tsx
+++ src/event-log-reader/event-card/EventsCounters.tsx
@@ -13,20 +13,44 @@ export class EventsCounter extends Component<IEventsCountersProps, IEventsCounte
     super(props)
   }
 
-  private getInfo(): string {
-    const res: any = Array.from(this.props.events).reduce((a,b)=>{
-      console.log('a:b',b[0], b[1])
-      return a+(` ${b[0]}: ${b[1]} `)
-    },'')
-    console.log(res)
-    return res;
+  private getIconStyle(event_type: string): {color: string, icon: string} {
+    const icons: {[index: string]: any} = {
+      'alarm': {color:'red', icon:'⬥'},
+      'warning': {color:'#ffba00', icon:'∎'},
+      'info': {color:'#8000C0', icon:'▲'},
+      'defaulf': ()=>{
+        console.log(`${event_type} not found`);
+        return {color:'gray', icon:'?'};
+      }
+    }
+    return (icons[event_type] || icons['default'])
   }
 
-  render () {
+  private getIconElement(event_type: string): JSX.Element {
+    const {color, icon} = {... this.getIconStyle(event_type)}
     return (
-      <span className="badge badge-primary badge-pill">
-        {this.getInfo()}
-    </span>
+      <span className="" style={{ color }}>{icon}</span>
+    )
+  }
+  
+  render () {
+    const items = Array.from(this.props.events).map((item) => {
+      return (
+        <div className='d-inline badge badge-pill badge-primary'>
+          {this.getIconElement(item[0])}
+          <span className="badge badge-primary badge-pill">{item[1]}</span>
+        </div>
+      )
+    })
+
+    return (
+      <div>{items}</div>
     )
   }
 }
+
+/*
+    if (mode === IEventSortMode.Alarm)   { return '⬥'}; "red" 
+    if (mode === IEventSortMode.Warning) { return '∎'}; "#ffba00"
+    if (mode === IEventSortMode.Info)    { return '▲'}; "#8000C0"
+*/
