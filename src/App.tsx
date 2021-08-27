86b66e6323631abb9e7f207f50bcbc55ede89619
Gerasim Gerasimov <dialix@yandex.ru>
2021-08-27 18:00:45 +0700

    это не код! TODO ошибка выскочила в event-logger-service, потерял связь с Tagger. Похоже ни тайм-аут ни фрэйм-енд не срабатывает после включения питания опрашиваемого девайса

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ import DateCardsContainer from './event-log-reader/date-cards-container/date-car
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import EventTablePage from './event-table/Pages/Events/event-table-page';
 
-
+/**TODO
+ВОт такая ошибка выскочила в event-logger-service, потерял связь с Tagger
+Я потрогал процессор на DExS.CPU там и Аджустере стало так что тоглаю бит Blank, и одновременно с ним CLM тоглается,
+а PWR вообще не тоглается. 
+event-logger-service > inner: Error: Fetch Error: network timeout at: http://localhost:5004/v1/devices/                       x
+xx event-logger-service >     at Function.<anonymous> (/usr/DExS/services/event-logger-service/src/http/client/controllers/devi  x
+event-logger-service >     at Generator.throw (<anonymous>)                                                                   x
+event-logger-service >     at rejected (/usr/DExS/services/event-logger-service/dist/http/client/controllers/device.js:6:65)  x
+event-logger-service >     at runMicrotasks (<anonymous>)                                                                     x
+event-logger-service >     at processTicksAndRejections (internal/process/task_queues.js:95:5)
+Состояние железа не должно влиять на сервис TAggerm так у него и загнузка на 100% возрасла, а к сожалению в лог я ничего не вывожу
+Возможно там проблема при преобразованиях в математике если DExS стал пургу гнать :-(
+ */
 
 /*TODO в EventTablePage обнаружен баг сортировки времени*/
 /*TODO в DateCardsContainer сделать сортировку и фильтрацию по Датам*/
